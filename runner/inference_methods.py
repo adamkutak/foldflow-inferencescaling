c8c391b46ebcb6ca54080e1a10e6b4a4d4d8e053
Adam Stcklov <adamkutak@gmail.com>
2025-06-10 20:28:58 -0400

    test

--- runner/inference_methods.py
+++ runner/inference_methods.py
@@ -697,7 +697,6 @@ class DivergenceFreeODEInference(InferenceMethod):
                             (rigids_tensor.shape[0],), t, device=device
                         )
 
-                        breakpoint()
                         # Generate divergence-free noise for rotation field
                         rot_divfree_noise = divfree_swirl_si(
                             rigids_tensor[
@@ -789,6 +788,7 @@ class DivergenceFreeODEInference(InferenceMethod):
                                 (rigids_tensor.shape[0],), t, device=device
                             )
 
+                            breakpoint()
                             # Generate divergence-free noise for rotation field
                             rot_divfree_noise = divfree_swirl_si(
                                 rigids_tensor[
