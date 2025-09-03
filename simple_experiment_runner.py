54887a2c68a1a1db103eb0ab4d2906f592de0987
Adam Stcklov <adamkutak@gmail.com>
2025-09-03 16:58:03 -0400

    test

--- simple_experiment_runner.py
+++ simple_experiment_runner.py
@@ -686,7 +686,7 @@ def main():
     parser.add_argument(
         "--num_samples",
         type=int,
-        default=20,
+        default=32,
         help="Number of samples to generate per method (default: 32)",
     )
 
