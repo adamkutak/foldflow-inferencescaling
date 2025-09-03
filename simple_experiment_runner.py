7d0ea026a4b05bdc2047e2fbc2a9b56707db38d8
Adam Stcklov <adamkutak@gmail.com>
2025-09-03 16:36:33 -0400

    test

--- simple_experiment_runner.py
+++ simple_experiment_runner.py
@@ -728,7 +728,7 @@ def main():
     parser.add_argument(
         "--massage_steps",
         type=int,
-        default=0,
+        default=1,
         help="Number of massaging steps to clean up noisy samples (default: 3, 0 to disable)",
     )
 
